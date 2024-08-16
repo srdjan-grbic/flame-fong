a033ddbf046f6577851bf9c9e7b971bb2a03ebe2
Srdjan Grbic <108718883+srdjan-grbic@users.noreply.github.com>
2024-08-16 22:05:51 +0200

    Initial step is done

---
+++ android/app/src/main/kotlin/com/example/flame_fong/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flame_fong
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/components/components.dart
@@ -0,0 +1,4 @@
+export 'ball.dart';
+export 'play_area.dart';
+export 'brick.dart';
+export 'bat.dart';
